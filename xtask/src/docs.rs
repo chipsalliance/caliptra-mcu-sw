57bebfe83343746760427cfeaf76b5bb85e5f05d
Christopher Swenson <chswenson@microsoft.com>
2024-10-30 15:02:48 -0700

    And mermaid too

--- xtask/src/docs.rs
+++ xtask/src/docs.rs
@@ -6,6 +6,7 @@ use crate::{DynError, PROJECT_ROOT};
 
 pub(crate) fn docs() -> Result<(), DynError> {
     check_mdbook()?;
+    check_mermaid()?;
     println!("Running: mdbook");
     let dir = PROJECT_ROOT.join("docs");
     let mut args = vec!["clippy", "--workspace"];
@@ -38,6 +39,26 @@ fn check_mdbook() -> Result<(), DynError> {
         .status()?;
     if !status.success() {
         Err("mdbook installation failed")?;
+    }
+    Ok(())
+}
+
+fn check_mermaid() -> Result<(), DynError> {
+    let status = Command::new("mdbook-mermaid")
+        .args(["--help"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status();
+    if status.is_ok() {
+        return Ok(());
+    }
+    println!("mdbook-mermaid not found; installing...");
+    let status = Command::new("cargo")
+        .current_dir(&*PROJECT_ROOT)
+        .args(["install", "mdbook-mermaid"])
+        .status()?;
+    if !status.success() {
+        Err("mdbook-mermaid installation failed")?;
     }
     Ok(())
 }
