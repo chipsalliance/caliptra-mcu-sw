e51a34582c45c01646919ba213cbd07c3ac72465
Vishal Soni <vsoni@microsoft.com>
2024-12-31 16:50:47 -0800

    PR Feedback

--- runtime/apps/apis/mctp/src/lib.rs
+++ runtime/apps/apis/mctp/src/lib.rs
@@ -9,13 +9,23 @@ use libtockasync::TockSubscribe;
 use core::fmt::Write;
 use libtock_console::Console;
 
+type EndpointId = u8;
+type Tag = u8;
+
 #[derive(Debug)]
 pub struct MessageInfo {
-    pub eid: u8,
-    pub msg_tag: u8,
-    pub msg_type: u8,
-    pub recv_time: u32,
-    pub payload_len: usize,
+    eid: EndpointId,
+    tag: Tag,
+}
+
+// impl from u32 for MessageInfo
+impl From<u32> for MessageInfo {
+    fn from(msg_info: u32) -> Self {
+        MessageInfo {
+            eid: ((msg_info & 0xFF0000) >> 16) as u8,
+            tag: (msg_info & 0xFF) as u8,
+        }
+    }
 }
 
 pub mod message_type {
@@ -26,31 +36,12 @@ pub mod message_type {
     pub const ANY_SUPPORTED: u8 = 0xFF; // Receive any supported message type
 }
 
-pub struct AsyncMctp<S: Syscalls, C: Config = DefaultConfig> {
+pub struct Mctp<S: Syscalls> {
     syscall: PhantomData<S>,
-    config: PhantomData<C>,
     driver_num: u32,
 }
 
-macro_rules! eid {
-    ($msg_info:expr) => {
-        (($msg_info & 0xFF0000) >> 16) as u8
-    };
-}
-
-macro_rules! msg_tag {
-    ($msg_info:expr) => {
-        ($msg_info & 0x07) as u8
-    };
-}
-
-macro_rules! msg_type {
-    ($msg_info:expr) => {
-        (($msg_info & 0xFF00) >> 8) as u8
-    };
-}
-
-impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
+impl<S: Syscalls> Mctp<S> {
     /// Create a new instance of the MCTP driver
     ///
     /// # Arguments
@@ -61,7 +52,6 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
     pub fn new(driver_num: u32) -> Self {
         Self {
             syscall: PhantomData,
-            config: PhantomData,
             driver_num,
         }
     }
@@ -76,53 +66,36 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
     /// Receive the MCTP request from the source EID
     ///
     /// # Arguments
-    /// * `source_eid` - The source EID from which the request is to be received.
     /// * `msg_payload` - The buffer to store the received message payload
     ///
     /// # Returns
     /// * `MessageInfo` - The message information containing the EID, message tag, message type, and payload length on success
     /// * `ErrorCode` - The error code on failure
-    pub async fn receive_request(
-        &self,
-        source_eid: u8,
-        msg_payload: &mut [u8],
-    ) -> Result<MessageInfo, ErrorCode> {
+    pub async fn receive_request(&self, req: &mut [u8]) -> Result<(u32, MessageInfo), ErrorCode> {
         let mut console_writer = Console::<S>::writer();
 
-        if msg_payload.is_empty() {
+        if req.is_empty() {
             writeln!(console_writer, "USER: Empty buffer!!!!").unwrap();
             return Err(ErrorCode::Invalid);
         }
-        writeln!(console_writer, "USER payload size {}", msg_payload.len()).unwrap();
+        writeln!(console_writer, "USER payload size {}", req.len()).unwrap();
 
-        let rw_sub = share::scope::<(), _, _>(|_handle| {
-            let rw_sub = TockSubscribe::subscribe_allow_rw::<S, C>(
+        let (recv_len, _, info) = share::scope::<(), _, _>(|_handle| {
+            let sub = TockSubscribe::subscribe_allow_rw::<S, DefaultConfig>(
                 self.driver_num,
                 subscribe::MESSAGE_RECEIVED,
                 allow_rw::MESSAGE_READ,
-                msg_payload,
+                req,
             );
 
-            S::command(
-                self.driver_num,
-                command::RECEIVE_REQUEST,
-                source_eid as u32,
-                MCTP_TAG_OWNER as u32,
-            )
-            .to_result::<(), ErrorCode>()?;
+            S::command(self.driver_num, command::RECEIVE_REQUEST, 0, 0)
+                .to_result::<(), ErrorCode>()?;
 
-            Ok(rw_sub)
-        })?;
+            Ok(sub)
+        })?
+        .await?;
 
-        writeln!(console_writer, "USER: AWAIT in Receive request").unwrap();
-        let (msg_len, recv_time, msg_info) = rw_sub.await?;
-        Ok(MessageInfo {
-            eid: eid!(msg_info),
-            msg_tag: msg_tag!(msg_info),
-            msg_type: msg_type!(msg_info),
-            recv_time: recv_time,
-            payload_len: msg_len as usize,
-        })
+        Ok((recv_len, info.into()))
     }
 
     /// Send the MCTP response to the destination EID
@@ -135,25 +108,20 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
     /// # Returns
     /// * `()` - On success
     /// * `ErrorCode` - The error code on failure
-    pub async fn send_response(
-        &self,
-        dest_eid: u8,
-        msg_tag: u8,
-        msg_payload: &[u8],
-    ) -> Result<(), ErrorCode> {
+    pub async fn send_response(&self, resp: &[u8], info: MessageInfo) -> Result<(), ErrorCode> {
         let ro_sub = share::scope::<(), _, _>(|_handle| {
-            let ro_sub = TockSubscribe::subscribe_allow_ro::<S, C>(
+            let ro_sub = TockSubscribe::subscribe_allow_ro::<S, DefaultConfig>(
                 self.driver_num,
                 subscribe::MESSAGE_TRANSMITTED,
                 allow_ro::MESSAGE_WRITE,
-                msg_payload,
+                resp,
             );
 
             S::command(
                 self.driver_num,
                 command::SEND_RESPONSE,
-                dest_eid as u32,
-                msg_tag as u32,
+                info.eid as u32,
+                info.tag as u32,
             )
             .to_result::<(), ErrorCode>()?;
 
@@ -177,13 +145,13 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
     /// # Returns
     /// * `u8` - The message tag assigned to the request
     /// * `ErrorCode` - The error code on failure
-    pub async fn send_request(&self, dest_eid: u8, msg_payload: &[u8]) -> Result<u8, ErrorCode> {
-        let ro_sub = share::scope::<(), _, _>(|_handle| {
-            let ro_sub = TockSubscribe::subscribe_allow_ro::<S, C>(
+    pub async fn send_request(&self, dest_eid: u8, req: &[u8]) -> Result<Tag, ErrorCode> {
+        let (result, _, info) = share::scope::<(), _, _>(|_handle| {
+            let sub = TockSubscribe::subscribe_allow_ro::<S, DefaultConfig>(
                 self.driver_num,
                 subscribe::MESSAGE_TRANSMITTED,
                 allow_ro::MESSAGE_WRITE,
-                msg_payload,
+                req,
             );
 
             S::command(
@@ -194,15 +162,16 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
             )
             .to_result::<(), ErrorCode>()?;
 
-            Ok(ro_sub)
-        })?;
+            Ok(sub)
+        })?
+        .await?;
 
-        ro_sub
-            .await
-            .map(|(result, _dest_eid, msg_info)| match result {
-                0 => Ok(((msg_info >> 8) & 0xFF) as u8),
-                _ => Err(result.try_into().unwrap_or(ErrorCode::Fail)),
-            })?
+        let info: MessageInfo = info.into();
+
+        match result {
+            0 => Ok(info.tag),
+            _ => Err(result.try_into().unwrap_or(ErrorCode::Fail)),
+        }
     }
 
     /// Receive the MCTP response from the source EID
@@ -217,12 +186,11 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
     /// * `ErrorCode` - The error code on failure
     pub async fn receive_response(
         &self,
-        source_eid: u8,
         msg_tag: u8,
         msg_payload: &mut [u8],
-    ) -> Result<MessageInfo, ErrorCode> {
-        let rw_sub = share::scope::<(), _, _>(|_handle| {
-            let rw_sub = TockSubscribe::subscribe_allow_rw::<S, C>(
+    ) -> Result<(u32, MessageInfo), ErrorCode> {
+        let (recv_len, _, info) = share::scope::<(), _, _>(|_handle| {
+            let sub = TockSubscribe::subscribe_allow_rw::<S, DefaultConfig>(
                 self.driver_num,
                 subscribe::MESSAGE_RECEIVED,
                 allow_rw::MESSAGE_READ,
@@ -232,46 +200,21 @@ impl<S: Syscalls, C: Config> AsyncMctp<S, C> {
             S::command(
                 self.driver_num,
                 command::RECEIVE_RESPONSE,
-                source_eid as u32,
+                0,
                 msg_tag as u32,
             )
             .to_result::<(), ErrorCode>()?;
 
-            Ok(rw_sub)
-        })?;
+            Ok(sub)
+        })?
+        .await?;
 
-        rw_sub
-            .await
-            .map(|(msg_len, recv_time, msg_info)| {
-                Ok(MessageInfo {
-                    eid: ((msg_info & 0xFF0000) >> 16) as u8,
-                    msg_tag: (msg_info & 0xFF) as u8,
-                    msg_type: (msg_info >> 8) as u8,
-                    recv_time,
-                    payload_len: msg_len as usize,
-                })
-            })
-            .map_err(|_| ErrorCode::Fail)?
+        Ok((recv_len, info.into()))
     }
 
-    pub fn get_max_message_size(&self) -> Result<u32, ErrorCode> {
+    pub fn max_message_size(&self) -> Result<u32, ErrorCode> {
         S::command(self.driver_num, command::GET_MAX_MESSAGE_SIZE, 0, 0).to_result()
     }
-}
-
-/// System call configuration trait for `mctp`.
-pub trait Config:
-    libtock_platform::allow_ro::Config
-    + libtock_platform::allow_rw::Config
-    + libtock_platform::subscribe::Config
-{
-}
-impl<
-        T: libtock_platform::allow_ro::Config
-            + libtock_platform::allow_rw::Config
-            + libtock_platform::subscribe::Config,
-    > Config for T
-{
 }
 
 // -----------------------------------------------------------------------------
@@ -282,9 +225,9 @@ const MCTP_TAG_OWNER: u8 = 0x08;
 
 pub mod driver_num {
     pub const MCTP_SPDM: u32 = 0xA0000;
-    pub const MCTP_SECURE_SPDM: u32 = 0xA0001;
+    pub const MCTP_SECURE: u32 = 0xA0001;
     pub const MCTP_PLDM: u32 = 0xA0002;
-    pub const MCTP_CALIPTRA_DRIVER_NUM: u32 = 0xA0003;
+    pub const MCTP_CALIPTRA: u32 = 0xA0003;
 }
 
 // Command IDs
