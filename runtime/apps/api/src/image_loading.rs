793c68e664e488b07c1d25804cd11170204df9c6
Marco Visaya <marcovisaya@microsoft.com>
2025-01-14 15:59:00 -0800

    Added the authorization step for the image

--- runtime/apps/api/src/image_loading.rs
+++ runtime/apps/api/src/image_loading.rs
@@ -1,12 +1,34 @@
+use crate::mailbox::MailboxRequestTrait;
+use crate::mailbox::{
+    AuthorizeAndStashRequest, GetImageLoadAddressRequest, GetImageLocationOffsetRequest,
+    GetImageSizeRequest, MailboxAPI, MailboxRequest, MailboxResponse, AUTHORIZED_IMAGE,
+};
+use libsyscall_caliptra::dma::{AXIAddr, DMASource, DMATransaction, DMA as DMASyscall};
+use libsyscall_caliptra::flash::{driver_num, SpiFlash as FlashSyscall};
 use libtock_platform::ErrorCode;
 use libtock_platform::Syscalls;
-use crate::mailbox::{GetImageLocationOffsetRequest, GetImageLoadAddressRequest, MailboxAPI, MailboxAPIIntf, MailboxRequest,MailboxResponse,
-GetImageSizeRequest};
-use libsyscall_caliptra::dma::{AXIAddr, DMASource, DMATransaction, DMA as DMASyscall};
-use libsyscall_caliptra::flash::{SpiFlash as FlashSyscall,driver_num};
 
-/// Trait defining the Image Loading module
-pub trait ImageLoaderApiIntf {
+pub struct ImageLoaderAPI<S: Syscalls> {
+    mailbox_api: MailboxAPI<S>,
+}
+
+/// This is the size of the buffer used for DMA transfers.
+const MAX_TRANSFER_SIZE: usize = 1024;
+
+impl<S: Syscalls> Default for ImageLoaderAPI<S> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<S: Syscalls> ImageLoaderAPI<S> {
+    /// Creates a new instance of the ImageLoaderAPI.
+    pub fn new() -> Self {
+        Self {
+            mailbox_api: MailboxAPI::new(),
+        }
+    }
+
     /// Loads the specified image to a storage mapped to the AXI bus memory map.
     ///
     /// # Parameters
@@ -15,98 +37,120 @@ pub trait ImageLoaderApiIntf {
     /// # Returns
     /// - `Ok()`: Image has been loaded and authorized succesfully.
     /// - `Err(ErrorCode)`: Indication of the failure to load or authorize the image.
-    async fn load_and_authorize(&self, image_id: u32) -> Result<(), ErrorCode>;
-}
-
-/// Implementation of the ImageLoaderAPI trait
-pub struct ImageLoaderAPI<S: Syscalls> {
-    mailbox_api: MailboxAPI<S>,
-}
-
-const MAX_DMA_TRANSFER_SIZE: usize = 1024;
-
-impl<S:Syscalls> ImageLoaderApiIntf for ImageLoaderAPI<S> {
-    async fn load_and_authorize(&self, image_id: u32) -> Result<(), ErrorCode> {
-        // Get image Offset
+    pub async fn load_and_authorize(&self, image_id: u32) -> Result<(), ErrorCode> {
         let offset = self.get_image_offset(image_id).await?;
-
-        // Get the image size
-        let img_size = self.get_image_image_size(image_id).await?;
-
-        // Get load address
+        let img_size = self.get_image_size(image_id).await?;
         let load_address = self.get_image_load_address(image_id).await?;
-
-        // Load image to the specified address
-        self.load_image(load_address, offset as usize, img_size).await?;
-
-        // Authorize the image
-
+        self.load_image(load_address, offset as usize, img_size)
+            .await?;
+        self.authorize_image(image_id).await?;
         Ok(())
     }
-}
 
-impl<S: Syscalls> ImageLoaderAPI<S> {
-    fn new() -> Self {
-        Self {
-            mailbox_api: MailboxAPI::new(),
-        }
-    }
-    
+    /// Retrieves the offset of the image in memory.
     async fn get_image_offset(&self, image_id: u32) -> Result<u32, ErrorCode> {
-        let request = GetImageLocationOffsetRequest::new(image_id);
-        let response = self.mailbox_api.execute_command(&MailboxRequest::GetImageLocationOffset(request)).await?;
-        if let MailboxResponse::GetImageLocationOffset(response) = response {
-            Ok(response.offset)
+        let mut request = GetImageLocationOffsetRequest {
+            fw_id: image_id.to_be_bytes(),
+            ..Default::default()
+        };
+        request.populate_checksum();
+        let response = self
+            .mailbox_api
+            .execute_command(&MailboxRequest::GetImageLocationOffset(request))
+            .await?;
+        if let MailboxResponse::GetImageLocationOffset(res) = response {
+            Ok(res.offset)
         } else {
             Err(ErrorCode::Fail)
         }
     }
 
+    /// Fetches the load address of the image.
     async fn get_image_load_address(&self, image_id: u32) -> Result<u64, ErrorCode> {
-        let request = GetImageLoadAddressRequest::new(image_id);
-        let response = self.mailbox_api.execute_command(&MailboxRequest::GetImageLoadAddress(request)).await?;
-        if let MailboxResponse::GetImageLoadAddress(response) = response {
-            Ok((response.load_address_high << 32 | response.load_address_low) as u64)
+        let mut request = GetImageLoadAddressRequest {
+            fw_id: image_id.to_be_bytes(),
+            ..Default::default()
+        };
+        request.populate_checksum();
+        let response = self
+            .mailbox_api
+            .execute_command(&MailboxRequest::GetImageLoadAddress(request))
+            .await?;
+        if let MailboxResponse::GetImageLoadAddress(res) = response {
+            Ok((res.load_address_high as u64) << 32 | res.load_address_low as u64)
         } else {
             Err(ErrorCode::Fail)
         }
     }
 
-    async fn get_image_image_size(&self, image_id: u32) -> Result<usize, ErrorCode> {
-        let request = GetImageSizeRequest::new(image_id);
-        let response = self.mailbox_api.execute_command(&MailboxRequest::GetImageSize(request)).await?;
-        if let MailboxResponse::GetImageSize(response) = response {
-            Ok(response.size as usize)
+    /// Retrieves the size of the image in bytes.
+    async fn get_image_size(&self, image_id: u32) -> Result<usize, ErrorCode> {
+        let mut request = GetImageSizeRequest {
+            fw_id: image_id.to_be_bytes(),
+            ..Default::default()
+        };
+        request.populate_checksum();
+        let response = self
+            .mailbox_api
+            .execute_command(&MailboxRequest::GetImageSize(request))
+            .await?;
+        if let MailboxResponse::GetImageSize(res) = response {
+            Ok(res.size as usize)
         } else {
             Err(ErrorCode::Fail)
         }
     }
 
-    async fn load_image(&self, load_address: AXIAddr, offset: usize, img_size: usize) -> Result<(), ErrorCode> {
+    /// Authorizes an image based on its ID.
+    async fn authorize_image(&self, image_id: u32) -> Result<(), ErrorCode> {
+        let mut request = AuthorizeAndStashRequest {
+            fw_id: image_id.to_be_bytes(),
+            ..Default::default()
+        };
+        request.populate_checksum();
+        let response = self
+            .mailbox_api
+            .execute_command(&MailboxRequest::AuthorizeAndStash(request))
+            .await?;
+        if let MailboxResponse::AuthorizeAndStash(res) = response {
+            if res.auth_req_result == AUTHORIZED_IMAGE {
+                return Ok(());
+            }
+        }
+        Err(ErrorCode::Fail)
+    }
+
+    /// Loads an image from flash into the specified address using DMA transfers.
+    async fn load_image(
+        &self,
+        load_address: AXIAddr,
+        offset: usize,
+        img_size: usize,
+    ) -> Result<(), ErrorCode> {
         let dma_syscall = DMASyscall::<S>::new();
         let flash_syscall = FlashSyscall::<S>::new(driver_num::IMAGE_PARTITION);
-        let mut buffer = [0; MAX_DMA_TRANSFER_SIZE];
+        let mut buffer = [0; MAX_TRANSFER_SIZE];
 
-        // Read a chunk from flash and transfer it through DMA until all the image chunks are copied
         let mut remaining_size = img_size;
         let mut current_offset = offset;
         let mut current_address = load_address;
+
         while remaining_size > 0 {
-            let transfer_size = if remaining_size > MAX_DMA_TRANSFER_SIZE { MAX_DMA_TRANSFER_SIZE } else { remaining_size };
-            flash_syscall.read(current_offset, transfer_size, &mut buffer).await?;
-            let transaction = DMATransaction{
+            let transfer_size = remaining_size.min(MAX_TRANSFER_SIZE);
+            flash_syscall
+                .read(current_offset, transfer_size, &mut buffer)
+                .await?;
+            let transaction = DMATransaction {
                 byte_count: transfer_size,
-                source: DMASource::Buffer(&mut buffer[..transfer_size]),
+                source: DMASource::Buffer(&buffer[..transfer_size]),
                 dest_addr: current_address,
             };
+            dma_syscall.xfer(&transaction).await?;
             remaining_size -= transfer_size;
             current_offset += transfer_size;
             current_address += transfer_size as u64;
-            dma_syscall.xfer(&transaction).await?;
         }
 
-
-        
         Ok(())
     }
 }
