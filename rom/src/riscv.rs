627ea5b32b440c4753e12b2fb74245e1d4e17228
Parvathi Bhogaraju <pbhogaraju@microsoft.com>
2025-05-22 12:51:36 -0700

    Remove debug format strings for error prints

--- rom/src/riscv.rs
+++ rom/src/riscv.rs
@@ -17,6 +17,7 @@ Abstract:
 use crate::fatal_error;
 use crate::fuses::Otp;
 use caliptra_api::mailbox::CommandId;
+use caliptra_api::CaliptraApiError;
 use core::{fmt::Write, hint::black_box};
 use registers_generated::{fuses::Fuses, i3c, mbox, mci, otp_ctrl, soc};
 use romtime::{HexWord, Mci, StaticRef, MCI_BASE};
@@ -132,9 +133,6 @@ pub const OTP_BASE: StaticRef<otp_ctrl::regs::OtpCtrl> =
 pub const I3C_BASE: StaticRef<i3c::regs::I3c> =
     unsafe { StaticRef::new(i3c::I3C_CSR_ADDR as *const i3c::regs::I3c) };
 
-pub const MBOX_BASE: StaticRef<mbox::regs::Mbox> =
-    unsafe { StaticRef::new(mbox::MBOX_CSR_ADDR as *const mbox::regs::Mbox) };
-
 pub fn rom_start() {
     romtime::println!("[mcu-rom] Hello from ROM");
 
@@ -178,14 +176,31 @@ pub fn rom_start() {
     if let Err(err) =
         soc.start_mailbox_req(CommandId::RI_DOWNLOAD_FIRMWARE.into(), 0, [].into_iter())
     {
-        romtime::println!("[mcu-rom] Error sending mailbox command: {:?}", err);
+        match err {
+            CaliptraApiError::MailboxCmdFailed(code) => {
+                romtime::println!("[mcu-rom] Error sending mailbox command: {}", HexWord(code));
+            }
+            _ => {
+                romtime::println!("[mcu-rom] Error sending mailbox command");
+            }
+        }
         fatal_error(4);
     }
     romtime::println!("[mcu-rom] Done sending RI_DOWNLOAD_FIRMWARE command");
     {
         // drop this to release the lock
         if let Err(err) = soc.finish_mailbox_resp(8, 8) {
-            romtime::println!("[mcu-rom] Error waiting for mailbox response: {:?}", err);
+            match err {
+                CaliptraApiError::MailboxCmdFailed(code) => {
+                    romtime::println!(
+                        "[mcu-rom] Error finishing mailbox command: {}",
+                        HexWord(code)
+                    );
+                }
+                _ => {
+                    romtime::println!("[mcu-rom] Error finishing mailbox command");
+                }
+            }
             fatal_error(5);
         }
     }
