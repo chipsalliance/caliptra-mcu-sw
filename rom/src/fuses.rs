e1c66f6db9e78002f3c2fddf198d540f26e76bdf
Christopher Swenson <chswenson@microsoft.com>
2025-07-13 20:46:14 -0700

    Add xtask for running a ROM on the FPGA without recompiling
    This makes it faster to do tests when changing the ROM, since you can
    compile locally, copy to the FPGA, and run without recompiling or
    relinking.
    
    I also fixed some of the logic that allows the FPGA ROM to trigger the
    FPGA hw model to exit correctly.
    
    This triggered some clippy warnings, which I cleaned up.

--- rom/src/fuses.rs
+++ rom/src/fuses.rs
@@ -19,7 +19,8 @@ impl Otp {
     }
 
     pub fn init(&self) -> Result<(), McuError> {
-        if self.registers.otp_status.get() & ((1 << 21) - 1) != 0 {
+        romtime::println!("Initializing OTP controller...");
+        if self.registers.otp_status.get() & ((1 << 22) - 1) != 0 {
             romtime::println!("OTP error: {}", self.registers.otp_status.get());
             return Err(McuError::FusesError);
         }
@@ -34,14 +35,19 @@ impl Otp {
             return Err(McuError::FusesError);
         }
 
-        // Disable periodic background checks
-        self.registers.consistency_check_period.set(0);
-        self.registers.integrity_check_period.set(0);
-        self.registers.check_timeout.set(0);
+        // Enable periodic background checks
+        // romtime::println!("Enabling consistency check period");
+        // self.registers.consistency_check_period.set(0x3ff_ffff);
+        romtime::println!("Enabling integrity check period");
+        self.registers.integrity_check_period.set(0x3ff_ffff);
+        romtime::println!("Enabling check timeout");
+        self.registers.check_timeout.set(0x10_0000);
         // Disable modifications to the background checks
+        romtime::println!("Disabling check modifications");
         self.registers
             .check_regwen
             .write(otp_ctrl::bits::CheckRegwen::Regwen::CLEAR);
+        romtime::println!("Done init");
         Ok(())
     }
 
@@ -63,7 +69,7 @@ impl Otp {
 
     /// Reads a word from the OTP controller.
     /// word_addr is in words
-    fn read_word(&self, word_addr: usize) -> Result<u32, McuError> {
+    pub fn read_word(&self, word_addr: usize) -> Result<u32, McuError> {
         // OTP DAI status should be idle
         while !self
             .registers
@@ -91,8 +97,71 @@ impl Otp {
         Ok(self.registers.dai_rdata_rf_direct_access_rdata_0.get())
     }
 
+    /// Write a word to the OTP controller.
+    /// word_addr is in words
+    pub fn write_word(&self, word_addr: usize, data: u32) -> Result<u32, McuError> {
+        // OTP DAI status should be idle
+        while !self
+            .registers
+            .otp_status
+            .is_set(otp_ctrl::bits::OtpStatus::DaiIdle)
+        {}
+
+        // load the data
+        self.registers.dai_wdata_rf_direct_access_wdata_0.set(data);
+
+        self.registers
+            .direct_access_address
+            .set((word_addr * 4) as u32);
+        // trigger a write
+        self.registers.direct_access_cmd.set(2);
+
+        // wait for DAI to go back to idle
+        while !self
+            .registers
+            .otp_status
+            .is_set(otp_ctrl::bits::OtpStatus::DaiIdle)
+        {}
+
+        if let Some(err) = self.check_error() {
+            romtime::println!("Error writing fuses: {}", HexWord(err));
+            self.print_errors();
+            return Err(McuError::FusesError);
+        }
+        Ok(self.registers.dai_rdata_rf_direct_access_rdata_0.get())
+    }
+
+    pub fn print_errors(&self) {
+        for i in 0..18 {
+            let err_code = match i {
+                0 => self.registers.err_code_rf_err_code_0.get(),
+                1 => self.registers.err_code_rf_err_code_1.get(),
+                2 => self.registers.err_code_rf_err_code_2.get(),
+                3 => self.registers.err_code_rf_err_code_3.get(),
+                4 => self.registers.err_code_rf_err_code_4.get(),
+                5 => self.registers.err_code_rf_err_code_5.get(),
+                6 => self.registers.err_code_rf_err_code_6.get(),
+                7 => self.registers.err_code_rf_err_code_7.get(),
+                8 => self.registers.err_code_rf_err_code_8.get(),
+                9 => self.registers.err_code_rf_err_code_9.get(),
+                10 => self.registers.err_code_rf_err_code_10.get(),
+                11 => self.registers.err_code_rf_err_code_11.get(),
+                12 => self.registers.err_code_rf_err_code_12.get(),
+                13 => self.registers.err_code_rf_err_code_13.get(),
+                14 => self.registers.err_code_rf_err_code_14.get(),
+                15 => self.registers.err_code_rf_err_code_15.get(),
+                16 => self.registers.err_code_rf_err_code_16.get(),
+                17 => self.registers.err_code_rf_err_code_17.get(),
+                _ => 0,
+            };
+            if err_code != 0 {
+                romtime::println!("OTP error code {}: {}", i, err_code);
+            }
+        }
+    }
+
     pub fn check_error(&self) -> Option<u32> {
-        let status = self.registers.otp_status.get() & 0x1fff;
+        let status = self.registers.otp_status.get() & 0x3ffff;
         if status == 0 {
             None
         } else {
