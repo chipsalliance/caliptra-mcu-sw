7be97ecd6cf9630986741d62142c27bc064bc24a
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2025-07-31 13:49:15 -0700

    test: warm boot flow
    Add simple test to trigger warm boot after exiting ROM and entering
    kernel.

--- tests/integration/src/lib.rs
+++ tests/integration/src/lib.rs
@@ -341,6 +341,8 @@ mod test {
     run_test!(test_doe_spdm_responder_conformance, nightly);
     run_test!(test_mci, example_app);
 
+    run_test!(test_warm_boot, example_app);
+
     /// This tests a full active mode boot run through with Caliptra, including
     /// loading MCU's firmware from Caliptra over the recovery interface.
     #[test]
