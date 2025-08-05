564092fc1e4e8e128a7d3847a90b424ba18fb35f
Parvathi Bhogaraju <pbhogaraju@microsoft.com>
2025-08-05 07:55:40 -0700

    increase timeout

--- emulator/app/src/tests/spdm_responder_validator/transport.rs
+++ emulator/app/src/tests/spdm_responder_validator/transport.rs
@@ -3,7 +3,7 @@
 pub const SOCKET_TRANSPORT_TYPE_MCTP: u32 = 0x01;
 pub const SOCKET_TRANSPORT_TYPE_PCI_DOE: u32 = 0x02;
 
-pub const MAX_CMD_TIMEOUT_SECONDS: u32 = 60;
+pub const MAX_CMD_TIMEOUT_SECONDS: u32 = 120;
 
 pub trait Transport {
     fn target_send_and_receive(&mut self, req: &[u8], wait_for_responder: bool) -> Option<Vec<u8>>;
