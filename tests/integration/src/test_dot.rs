e581c1b5d4cbb2a79d97ab864d50b45330d045e3
Christopher Swenson <chswenson@microsoft.com>
2025-12-09 14:09:20 -0800

    Start on DOT blob calculation in hw model

--- tests/integration/src/test_dot.rs
+++ tests/integration/src/test_dot.rs
@@ -4,9 +4,100 @@
 
 #[cfg(test)]
 mod test {
+    use std::{
+        collections::HashMap,
+        sync::{LazyLock, Mutex},
+    };
+
     use crate::test::{start_runtime_hw_model, TestParams, TEST_LOCK};
+    use caliptra_api::{
+        calc_checksum,
+        mailbox::{
+            CmDeriveStableKeyReq, CmDeriveStableKeyResp, CmHashAlgorithm, CmHmacReq, CmHmacResp,
+            CmStableKeyType, CommandId, CMB_HMAC_MAX_SIZE,
+        },
+        SocManager,
+    };
+    use caliptra_hw_model::HwModel;
     use mcu_error::McuError;
     use mcu_hw_model::McuHwModel;
+    use mcu_rom_common::{BootFlow, McuRomBootStatus};
+    use zerocopy::{FromBytes, IntoBytes};
+
+    static HMACS: LazyLock<Mutex<HashMap<Vec<u8>, Vec<u8>>>> =
+        LazyLock::new(|| Mutex::new(HashMap::new()));
+
+    fn compute_hmac_cached(blob: &[u8]) -> Vec<u8> {
+        let mut hmacs = HMACS.lock().unwrap();
+
+        match hmacs.get(blob) {
+            Some(h) => h.clone(),
+            None => {
+                let h = compute_hmac(blob);
+                hmacs.insert(blob.to_vec(), h.clone());
+                h
+            }
+        }
+    }
+
+    fn compute_hmac(blob: &[u8]) -> Vec<u8> {
+        let lock = TEST_LOCK.lock().unwrap();
+        lock.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+
+        let mut hw = start_runtime_hw_model(TestParams {
+            rom_only: true,
+            ..Default::default()
+        });
+
+        hw.step_until(|m| {
+            (m.mci_flow_status() & 0xffff) as u16
+                >= McuRomBootStatus::CaliptraReadyForMailbox.into()
+        });
+
+        // TODO: derive stable key
+        let mut req = CmDeriveStableKeyReq {
+            key_type: CmStableKeyType::IDevId.into(),
+            ..Default::default()
+        };
+        let label = [0u8; 32];
+        req.info[..23].copy_from_slice(b"Caliptra DOT stable key");
+        let req = req.as_mut_bytes();
+        calc_checksum(CommandId::CM_DERIVE_STABLE_KEY.into(), req);
+
+        let result = hw
+            .mailbox_execute(CommandId::CM_DERIVE_STABLE_KEY.into(), req)
+            .unwrap()
+            .unwrap();
+        let resp = CmDeriveStableKeyResp::read_from_bytes(&result).unwrap();
+        let cmk = resp.cmk;
+
+        let mut req = CmHmacReq {
+            cmk,
+            hash_algorithm: CmHashAlgorithm::Sha512.into(),
+            data_size: blob.len() as u32,
+            ..Default::default()
+        };
+        req.data[..blob.len()].copy_from_slice(blob);
+
+        let req = req.as_mut_bytes();
+        calc_checksum(CommandId::CM_HMAC.into(), req);
+
+        let result = hw
+            .mailbox_execute(CommandId::CM_HMAC.into(), req)
+            .unwrap()
+            .unwrap();
+        let resp = CmHmacResp::read_from_bytes(&result).unwrap();
+
+        // force the compiler to keep the lock
+        lock.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        resp.mac.to_vec()
+    }
+
+    #[test]
+    fn test_dot_blob_valid() {
+        let blob = [0u8; 32]; // TODO: make a valid DOT blob
+        let hmac = compute_hmac_cached(&blob);
+    }
 
     #[test]
     fn test_dot_blob_corrupt() {
