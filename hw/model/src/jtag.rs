0484d3bcbf78d4c359475e77540e7b75cc5568c2
Tim Trippel <ttrippel@google.com>
2026-01-26 16:15:57 -0800

    [jtag] add timeouts to JTAG TAP commands
    This Adds proper timeout logic to the "acquire lock" and "wait for
    response" mailbox commands so tests do not have to wait for the much
    longer test timeouts.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- hw/model/src/jtag.rs
+++ hw/model/src/jtag.rs
@@ -1,7 +1,7 @@
 // Licensed under the Apache-2.0 license
 
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
 use caliptra_api::checksum::calc_checksum;
 use caliptra_api::mailbox::CommandId;
@@ -15,25 +15,29 @@ use zerocopy::{FromBytes, IntoBytes};
 #[repr(u32)]
 #[derive(Debug, PartialEq, IntEnum)]
 pub enum CoreMailboxStatus {
-    CoreMailboxStatusCmdBusy = 0x0,
-    CoreMailboxStatusDataReady = 0x1,
-    CoreMailboxStatusCmdComplete = 0x2,
-    CoreMailboxStatusCmdFailure = 0x3,
+    CmdBusy = 0x0,
+    DataReady = 0x1,
+    CmdComplete = 0x2,
+    CmdFailure = 0x3,
 }
+
+const TIMEOUT_MS: Duration = Duration::from_millis(1000); // 1 second
 
 /// Acquire Caliptra Core mailbox lock over JTAG TAP.
 ///
 /// This function blocks until the lock is acquired.
 pub fn jtag_acquire_caliptra_mailbox_lock(tap: &mut OpenOcdJtagTap) -> Result<()> {
-    loop {
+    let now = SystemTime::now();
+    while now.elapsed().unwrap() < TIMEOUT_MS {
         let mbox_lock = tap.read_reg(&CaliptraCoreReg::MboxLock)?;
         if (mbox_lock & 0x1) == 0 {
             return Ok(());
         }
         thread::sleep(Duration::from_millis(100));
     }
-
-    // TODO: add timeout handling
+    Err(anyhow!(
+        "Timeout: waiting to acquire Caliptra Core mailbox."
+    ))
 }
 
 /// Send a mailbox command to Caliptra Core over JTAG TAP.
@@ -71,17 +75,19 @@ pub fn jtag_send_caliptra_mailbox_cmd(
 ///
 /// Returns the mbox_status.status bit field.
 pub fn jtag_wait_for_caliptra_mailbox_resp(tap: &mut OpenOcdJtagTap) -> Result<CoreMailboxStatus> {
-    loop {
+    let now = SystemTime::now();
+    while now.elapsed().unwrap() < TIMEOUT_MS {
         let mbox_status =
             CoreMailboxStatus::try_from(tap.read_reg(&CaliptraCoreReg::MboxStatus)? & 0xf)
                 .expect("Invalid Caliptra Core mailbox status.");
-        if mbox_status != CoreMailboxStatus::CoreMailboxStatusCmdBusy {
+        if mbox_status != CoreMailboxStatus::CmdBusy {
             return Ok(mbox_status);
         }
         thread::sleep(Duration::from_millis(100));
     }
-
-    // TODO: add timeout handling
+    Err(anyhow!(
+        "Timeout: waiting to acquire Caliptra Core mailbox."
+    ))
 }
 
 /// Get Caliptra Core mailbox response over JTAG TAP.
@@ -91,7 +97,7 @@ pub fn jtag_wait_for_caliptra_mailbox_resp(tap: &mut OpenOcdJtagTap) -> Result<C
 /// Returns the response as a vector of bytes.
 pub fn jtag_get_caliptra_mailbox_resp(tap: &mut OpenOcdJtagTap) -> Result<Vec<u8>> {
     let mbox_status = jtag_wait_for_caliptra_mailbox_resp(tap)?;
-    if mbox_status != CoreMailboxStatus::CoreMailboxStatusDataReady {
+    if mbox_status != CoreMailboxStatus::DataReady {
         return Err(anyhow!(
             "No response data in the mailbox (status = {:?}).",
             mbox_status
