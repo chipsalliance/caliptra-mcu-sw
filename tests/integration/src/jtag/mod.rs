17c53e36f80cf6572a56dd791278a781c88b11fe
Amit Kumar-Hermosillo <amitkh@google.com>
2026-02-18 20:47:17 +0000

    Run RV32 instructions out of SRAM
    after entering debug mode
    
    Signed-off-by: Amit Kumar-Hermosillo <amitkh@google.com>

--- tests/integration/src/jtag/mod.rs
+++ tests/integration/src/jtag/mod.rs
@@ -9,7 +9,9 @@ mod test_uds;
 #[cfg(test)]
 mod test {
 
-    use caliptra_hw_model::jtag::DmReg;
+    use std::time::Duration;
+
+    use caliptra_hw_model::jtag::{CsrReg, DmReg};
     use caliptra_hw_model::openocd::openocd_jtag_tap::OpenOcdJtagTap;
     use caliptra_hw_model::Fuses;
     use mcu_builder::FirmwareBinaries;
@@ -17,7 +19,9 @@ mod test {
     use mcu_hw_model::{DefaultHwModel, InitParams, McuHwModel};
     use romtime::LifecycleControllerState;
 
-    use anyhow::Result;
+    use anyhow::{anyhow, Result};
+
+    pub const ALLHALTED_MASK: u32 = 1 << 9;
 
     pub fn ss_setup(
         initial_lc_state: Option<LifecycleControllerState>,
@@ -46,22 +50,58 @@ mod test {
     }
 
     /// Write/Read words to SRAM over the system bus.
-    fn sysbus_write_read(tap: &mut OpenOcdJtagTap, sram_base_addr: u32) -> Result<bool> {
-        const NUM_WORDS: u32 = 4;
-        for i in 0..NUM_WORDS {
-            let addr = sram_base_addr + i * 4;
-            let value = 0xa5a5a5a5;
-            tap.write_memory_32(addr, value)?;
+    pub fn sysbus_write_read(
+        tap: &mut OpenOcdJtagTap,
+        sram_base_addr: u32,
+        data: &[u32],
+    ) -> Result<bool> {
+        for i in 0..data.len() {
+            let addr = sram_base_addr + u32::try_from(i)? * 4;
+            tap.write_memory_32(addr, data[i])?;
             let read_value = tap.read_memory_32(addr)?;
             println!(
                 "Wrote 0x{:x} to 0x{:x}; Read 0x{:x}",
-                value, addr, read_value
+                data[i], addr, read_value
             );
-            if value != read_value {
+            if data[i] != read_value {
                 return Ok(false);
             }
         }
         Ok(true)
+    }
+
+    /// Poll until a given mask is set in the dmstatus register
+    pub fn wait_status(tap: &mut OpenOcdJtagTap, mask: u32, timeout: Duration) -> Result<()> {
+        for _ in 0..100 {
+            let status = tap.read_reg(&DmReg::DmStatus)?;
+            if status & mask > 0 {
+                return Ok(());
+            }
+            std::thread::sleep(timeout / 100);
+        }
+
+        Err(anyhow!("Timed out waiting for status register"))
+    }
+
+    /// Send a halt request to the dmcontrol register
+    pub fn halt(tap: &mut OpenOcdJtagTap) -> Result<()> {
+        const HALT_REQ: u32 = 1 << 31 | 1;
+        tap.write_reg(&DmReg::DmControl, HALT_REQ)
+    }
+
+    /// Send a resume request to the dmcontrol register
+    pub fn resume(tap: &mut OpenOcdJtagTap) -> Result<()> {
+        const RESUME_REQ: u32 = 1 << 30 | 1;
+        tap.write_reg(&DmReg::DmControl, RESUME_REQ)
+    }
+
+    /// Write a value to a CSR using the abstract command register
+    pub fn write_csr_reg(tap: &mut OpenOcdJtagTap, reg: CsrReg, value: u32) -> Result<()> {
+        tap.write_reg(&DmReg::DmAbstractData0, value)?;
+
+        // From https://chipsalliance.github.io/Cores-VeeR-EL2/html/main/docs_rendered/html/debugging.html#abstract-command-register-command
+        let abstract_cmd = 0x00230000 | (reg as u32 & 0xFFF);
+        tap.write_reg(&DmReg::DmAbstractCommand, abstract_cmd)
     }
 
     /// Check if a debug module is active.
@@ -89,7 +129,7 @@ mod test {
         }
 
         // Test writes to Caliptra MCU SRAM.
-        if !sysbus_write_read(mcu_tap, FPGA_MEMORY_MAP.sram_offset)? {
+        if !sysbus_write_read(mcu_tap, FPGA_MEMORY_MAP.sram_offset, &[0xa5a5a5a5])? {
             return Ok(false);
         }
 
