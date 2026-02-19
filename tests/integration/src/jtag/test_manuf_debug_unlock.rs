bbf3f91413c6edaacade52f0667112d3d394a72e
Amit Kumar-Hermosillo <amitkh@google.com>
2026-02-19 15:49:27 +0000

    Enable flow in prod_debug_unlock test
    and move some of the code up a layer so it's conducive to that
    
    Signed-off-by: Amit Kumar-Hermosillo <amitkh@google.com>

--- tests/integration/src/jtag/test_manuf_debug_unlock.rs
+++ tests/integration/src/jtag/test_manuf_debug_unlock.rs
@@ -6,34 +6,17 @@ mod test {
     use std::thread;
     use std::time::Duration;
 
-    use crate::jtag::test::{
-        debug_is_unlocked, halt, resume, ss_setup, sysbus_write_read, wait_status, write_csr_reg,
-        ALLHALTED_MASK,
-    };
+    use crate::jtag::test::{debug_is_unlocked, ss_setup, verify_execute_from_sram};
 
     use caliptra_api::mailbox::CommandId;
-    use caliptra_hw_model::jtag::{CaliptraCoreReg, CsrReg};
+    use caliptra_hw_model::jtag::CaliptraCoreReg;
     use caliptra_hw_model::openocd::openocd_jtag_tap::{JtagParams, JtagTap};
     use caliptra_hw_model::HwModel;
     use caliptra_hw_model::DEFAULT_MANUF_DEBUG_UNLOCK_RAW_TOKEN;
-    use mcu_config_fpga::FPGA_MEMORY_MAP;
     use mcu_hw_model::jtag::{jtag_get_caliptra_mailbox_resp, jtag_send_caliptra_mailbox_cmd};
     use romtime::LifecycleControllerState;
 
     use zerocopy::IntoBytes;
-
-    const PRG_OFFSET: u32 = 0x100;
-    const PRG_ADDRESS: u32 = FPGA_MEMORY_MAP.sram_offset | PRG_OFFSET;
-    const RV32_INSTS: &[u32] = &[
-        0x00100513,                                                             // addi x10, x0, 1
-        0x00200593,                                                             // addi x11, x0, 2
-        0x00b50533,                                                             // add x10, x10, x11
-        0x00000637 | (FPGA_MEMORY_MAP.sram_offset & 0xFFFFF000), // lui x12 0xa8c00 (sram base addr)
-        0x00a62023 | (PRG_OFFSET & 0x1F) << 7 | (PRG_OFFSET >> 5 & 0x7F) << 25, // sw x10, 0x100(x12)
-        0x00100073,                                                             // ebreak
-    ];
-
-    const DCSR_SET_EBREAKM: u32 = 0x8003; // to enable debug mode on an ebreak
 
     #[test]
     fn test_manuf_debug_unlock() {
@@ -123,31 +106,6 @@ mod test {
         let is_unlocked = debug_is_unlocked(&mut *core_tap, &mut *mcu_tap).unwrap_or(false);
         assert_eq!(is_unlocked, true);
 
-        halt(&mut mcu_tap).expect("Failed to halt MCU");
-
-        // Write program to SRAM (MCU should be halted)
-        assert!(matches!(
-            sysbus_write_read(&mut *mcu_tap, FPGA_MEMORY_MAP.sram_offset, RV32_INSTS),
-            Ok(true)
-        ));
-
-        // set PC to start of SRAM
-        write_csr_reg(&mut mcu_tap, CsrReg::Dpc, FPGA_MEMORY_MAP.sram_offset)
-            .expect("Failed to set PC");
-
-        // Write DCSR to enable ebreakm (bit 15) so ebreak enters debug mode
-        write_csr_reg(&mut mcu_tap, CsrReg::Dcsr, DCSR_SET_EBREAKM)
-            .expect("Failed to set DCSR to enable debug mode on ebreak");
-
-        resume(&mut mcu_tap).expect("Failed to resume MCU");
-        wait_status(&mut mcu_tap, ALLHALTED_MASK, Duration::from_secs(3))
-            .expect("MCU did not halt after executing instructions");
-
-        assert_eq!(
-            mcu_tap
-                .read_memory_32(PRG_ADDRESS)
-                .expect("Failed to readback from MCU SRAM"),
-            3
-        )
+        verify_execute_from_sram(&mut mcu_tap).expect("Failed to execute program from SRAM");
     }
 }
