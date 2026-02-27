a42eda5eb4e0fc9b05dea69897de3a62803a62f5
Todd M. Ernst <ternst@nvidia.com>
2026-02-27 12:58:42 -0800

    ocp: Add DeviceReset block structure (#1028)
    Add the DeviceReset command block to the protocol implementation with
    appropriate unit tests.  As it is both read and write add serialization
    and deserialization methods.

--- common/ocp/src/error.rs
+++ common/ocp/src/error.rs
@@ -14,4 +14,10 @@ pub enum OcpError {
     ProtCapRecoveryMemoryAccessRequired = 3,
     /// The provided buffer is too small to hold the serialized message.
     BufferTooSmall = 4,
+    /// DEVICE_RESET: Byte pattern in the Device Reset is invalid.
+    DeviceResetInvalid = 5,
+    /// Message slice is too short for the expected command.
+    MessageTooShort = 6,
+    /// Message slice is longer than the expected command.
+    MessageTooLong = 7,
 }
