fc100fb5f5f0f11187102516a5c7f46338d58834
Todd M. Ernst <ternst@nvidia.com>
2026-03-03 08:07:40 -0800

    ocp: Add indirect fifo status to protocol (#781)
    Add the indirect fifo status data structure to the protocol library.

--- common/ocp/src/error.rs
+++ common/ocp/src/error.rs
@@ -38,4 +38,6 @@ pub enum OcpError {
     IndirectStatusInvalidCmsRegionType = 15,
     /// INDIRECT_FIFO_CTRL: reserved value in Reset field (byte 1).
     IndirectFifoCtrlInvalidReset = 16,
+    /// INDIRECT_FIFO_STATUS: reserved CMS region type value (byte 1, bits 0-2).
+    IndirectFifoStatusInvalidRegionType = 17,
 }
