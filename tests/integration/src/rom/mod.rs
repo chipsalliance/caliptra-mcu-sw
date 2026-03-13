0383f7302f37c66091d370f0f0e4b35a8b077ebd
Amit Kumar-Hermosillo <amitkh@google.com>
2026-03-13 17:33:44 +0000

    Add integration test for BootFSM timeout
    FPGA only, emulator cannot halt bootfsm
    
    Signed-off-by: Amit Kumar-Hermosillo <amitkh@google.com>

--- tests/integration/src/rom/mod.rs
+++ tests/integration/src/rom/mod.rs
@@ -3,3 +3,8 @@
 mod test_hitless_update;
 mod test_sw_digest_lock;
 mod test_warm_reset;
+
+// testing this requires enabling the BootFSM breakpoint which is only implemented
+// on FPGA for now.
+#[cfg(feature = "fpga_realtime")]
+mod test_bootfsm_timeout;
