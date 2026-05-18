bfcfe5c37ba606e19621450b4fb90195250c9275
Amit Kumar-Hermosillo <amitkh@google.com>
2026-05-18 16:10:34 +0000

    [bare-metal] move away from runtime paradigm to support FPGA

--- runtime/bare-metal/src/main.rs
+++ runtime/bare-metal/src/main.rs
@@ -6,38 +6,50 @@
 #![no_main]
 
 #[cfg(target_arch = "riscv32")]
-use core::arch::global_asm;
+mod riscv {
+    use core::arch::global_asm;
+    use core::panic::PanicInfo;
 
-#[cfg(target_arch = "riscv32")]
-use core::panic::PanicInfo;
+    global_asm!(include_str!("start.S"));
 
-#[cfg(target_arch = "riscv32")]
-global_asm!(include_str!("start.S"));
-
-#[cfg(target_arch = "riscv32")]
-const MSG: &[u8; 31] = b"Hello from Bare Metal Runtime!\n";
-
-#[cfg(target_arch = "riscv32")]
-#[no_mangle]
-pub extern "C" fn main() {
-    // Write the message to the console.
+    const FPGA_UART_OUTPUT: *mut u32 = 0xa401_1014 as *mut u32;
     const UART0: *mut u8 = 0x1000_1041 as *mut u8;
-    unsafe {
-        for byte in MSG {
-            core::ptr::write_volatile(UART0, *byte);
-        }
-        core::ptr::write_volatile(UART0, b'\n');
-    }
-    // Exit the emulator.
-    unsafe {
-        core::ptr::write_volatile(0x1000_2000 as *mut u32, 0);
-    }
-}
+    const EMU_CTRL_EXIT: *mut u32 = 0x1000_2000 as *mut u32;
 
-#[cfg(target_arch = "riscv32")]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    const MSG: &[u8] = b"Hello from Bare Metal Runtime!\n";
+
+    unsafe fn write_byte(byte: u8) {
+        if cfg!(feature = "fpga") {
+            core::ptr::write_volatile(FPGA_UART_OUTPUT, byte as u32 | 0x100);
+        } else {
+            core::ptr::write_volatile(UART0, byte);
+        }
+    }
+
+    unsafe fn exit_runtime() -> ! {
+        if cfg!(feature = "fpga") {
+            // Signal exit success over UART for direct simulation runs
+            core::ptr::write_volatile(FPGA_UART_OUTPUT, 0xff | 0x100);
+        } else {
+            core::ptr::write_volatile(EMU_CTRL_EXIT, 0);
+        }
+        loop {}
+    }
+
+    #[no_mangle]
+    pub extern "C" fn main() {
+        unsafe {
+            for &byte in MSG {
+                write_byte(byte);
+            }
+            exit_runtime();
+        }
+    }
+
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
 }
 
 // Dummy main for non-RISC-V targets.
