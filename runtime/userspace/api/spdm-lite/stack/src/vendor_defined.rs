eb1cf7a483021f1361df6d8b9e472ad8c4b914f6
Arthur Heymans <arthur@aheymans.xyz>
2026-05-27 17:42:07 +0200

    spdm-lite: fix OCP VDM review findings

--- runtime/userspace/api/spdm-lite/stack/src/vendor_defined.rs
+++ runtime/userspace/api/spdm-lite/stack/src/vendor_defined.rs
@@ -10,7 +10,7 @@ use mcu_spdm_lite_traits::{PalBytes, SpdmPal, SpdmPalIo, SpdmPalIoKind, SpdmPalI
 use zerocopy::{little_endian::U16, FromBytes};
 
 use crate::build::build_error_response;
-use crate::chunk::effective_data_transfer_size;
+use crate::chunk::{effective_data_transfer_size, effective_max_spdm_msg_size};
 use crate::error::{
     SpdmResult, SPDM_INVALID_REQUEST, SPDM_LARGE_RESPONSE, SPDM_UNEXPECTED_REQUEST,
     SPDM_UNSPECIFIED, SPDM_UNSUPPORTED_REQUEST, SPDM_VERSION_MISMATCH,
@@ -217,7 +217,7 @@ where
     }
 
     let head = pal.header_size();
-    let mut rsp = pal.alloc_bytes(io, head + fixed_len + max_rsp_len)?;
+    let mut rsp = alloc_padded(pal, io, head + fixed_len + max_rsp_len)?;
     let payload_len_pos = payload_len_offset(vendor_id_len);
     let payload_len_offset = head + payload_len_pos;
     let payload_offset = payload_len_offset + core::mem::size_of::<U16>();
@@ -243,7 +243,7 @@ where
             .handle_request(
                 req,
                 VdmResponseBuffers {
-                    inline: &mut rsp[payload_offset..],
+                    inline: &mut rsp[payload_offset..payload_offset + max_rsp_len],
                     large: Some(large_payload),
                 },
             )
@@ -254,6 +254,7 @@ where
                     &mut rsp,
                     head,
                     fixed_len,
+                    pal,
                     payload_len_offset,
                     max_rsp_len,
                     rsp_len,
@@ -274,7 +275,7 @@ where
             .handle_request(
                 req,
                 VdmResponseBuffers {
-                    inline: &mut rsp[payload_offset..],
+                    inline: &mut rsp[payload_offset..payload_offset + max_rsp_len],
                     large: None,
                 },
             )
@@ -285,6 +286,7 @@ where
                     &mut rsp,
                     head,
                     fixed_len,
+                    pal,
                     payload_len_offset,
                     max_rsp_len,
                     rsp_len,
@@ -298,13 +300,13 @@ where
     if large_total_len <= fixed_len + max_rsp_len {
         let large_rsp = pal.large_message(large_total_len)?;
         rsp[head..head + large_total_len].copy_from_slice(large_rsp);
-        Pal::shrink_bytes(&mut rsp, head + large_total_len)?;
+        shrink_padded(pal, &mut rsp, head + large_total_len)?;
         return Ok(rsp);
     }
 
     let chunking =
         state.cap_flags.contains(CapFlags::CHUNK) && state.peer_cap_flags.contains(CapFlags::CHUNK);
-    if !chunking {
+    if !chunking || large_total_len > effective_max_spdm_msg_size(state, pal) {
         return Err(SPDM_UNSPECIFIED);
     }
     let handle = state.large_response.start_buffered(large_total_len);
@@ -350,6 +352,7 @@ fn finish_inline_response<Pal: SpdmPal>(
     rsp: &mut PalBytes<'_, Pal>,
     head: usize,
     fixed_len: usize,
+    pal: &Pal,
     payload_len_offset: usize,
     max_rsp_len: usize,
     rsp_len: usize,
@@ -359,6 +362,41 @@ fn finish_inline_response<Pal: SpdmPal>(
     }
     rsp[payload_len_offset..payload_len_offset + core::mem::size_of::<U16>()]
         .copy_from_slice(&(rsp_len as u16).to_le_bytes());
-    Pal::shrink_bytes(rsp, head + fixed_len + rsp_len)?;
+    shrink_padded(pal, rsp, head + fixed_len + rsp_len)?;
+    Ok(())
+}
+
+fn padded_len<Pal: SpdmPal>(pal: &Pal, raw_len: usize) -> SpdmResult<usize> {
+    let align = pal.send_len_alignment();
+    debug_assert!(align > 0 && align.is_power_of_two());
+    raw_len
+        .checked_add(align - 1)
+        .map(|len| len & !(align - 1))
+        .ok_or(SPDM_UNSPECIFIED)
+}
+
+fn alloc_padded<'a, Pal: SpdmPal>(
+    pal: &'a Pal,
+    io: &Pal::Io<'_>,
+    raw_len: usize,
+) -> SpdmResult<PalBytes<'a, Pal>> {
+    let alloc_len = padded_len(pal, raw_len)?;
+    let mut buf = pal.alloc_bytes(io, alloc_len)?;
+    for b in &mut buf[raw_len..alloc_len] {
+        *b = 0;
+    }
+    Ok(buf)
+}
+
+fn shrink_padded<Pal: SpdmPal>(
+    pal: &Pal,
+    rsp: &mut PalBytes<'_, Pal>,
+    raw_len: usize,
+) -> SpdmResult<()> {
+    let shrink_len = padded_len(pal, raw_len)?;
+    for b in &mut rsp[raw_len..shrink_len] {
+        *b = 0;
+    }
+    Pal::shrink_bytes(rsp, shrink_len)?;
     Ok(())
 }
