97e84ea6f08fcb0e93092bdba6b0c804798f900f
Marco Visaya <marcovisaya@microsoft.com>
2026-05-28 15:39:16 -0700

    [auth-cmds] Add a command to get a challenge nonce (#1358)
    Cherry-pick of 823a766f from main, adapted for main-2.1:
    - Fixed crate rename: external-cmds-common -> caliptra-common-commands
    - Fixed trait rename: UnifiedCommandHandler -> CaliptraCmdHandler (from #1351)
    - Dropped FuseIncreaseCaliptraMinSvn/FeProg/ExportAttestedCsr entries (not in main-2.1)
    - Used core::mem::size_of_val for stable Rust compatibility

--- platforms/emulator/runtime/userspace/apps/user/src/mcu_mbox/cmd_auth_mock.rs
+++ platforms/emulator/runtime/userspace/apps/user/src/mcu_mbox/cmd_auth_mock.rs
@@ -1,14 +1,25 @@
 // Licensed under the Apache-2.0 license
-use caliptra_mcu_external_cmds_common::CommandAuthorizer;
+use caliptra_mcu_common_commands::CommandAuthorizer;
 
-pub struct MockCommandAuthorizer;
+#[derive(Default)]
+pub struct MockCommandAuthorizer {
+    challenge: Option<[u8; 32]>,
+}
 
 impl CommandAuthorizer for MockCommandAuthorizer {
     fn is_authorized<'a>(
         &self,
         _cmd_id: caliptra_mcu_mbox_common::messages::CommandId,
         req: &'a [u8],
-    ) -> Result<&'a [u8], caliptra_mcu_external_cmds_common::AuthorizationError> {
+    ) -> Result<&'a [u8], caliptra_mcu_common_commands::AuthorizationError> {
         Ok(req)
+    }
+
+    fn take_challenge(&mut self) -> Option<[u8; 32]> {
+        self.challenge.take()
+    }
+
+    fn set_challenge(&mut self, challenge: [u8; 32]) {
+        self.challenge = Some(challenge)
     }
 }
