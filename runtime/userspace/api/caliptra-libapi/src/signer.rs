a0064fb5c1265ff2e8d5ea692d14eb9d3836293d
Arul Srivastava <srivastavaarul@google.com>
2026-08-20 16:06:11 +0000

    ocp-lock: support ML-DSA keys from DPE for endorsement certs
    Implement EndorsementAlgorithm enum to support both ECDSA P-384 and ML-DSA-87
    (FIPS 204) signatures from DPE for OCP LOCK endorsement certificates.
    
    - Add CaliptraDpeSigner::with_algorithm for selecting the signing algorithm.
    - Dispatch SIGN_WITH_EXPORTED_MLDSA commands with MldsaSignType::Mu.
    - Update X.509 certificate builder with standard ML-DSA-87
      and direct BIT STRING signature encoding.
    - Expand max endorsement certificate buffer to 8 KB.
    
    Fixes #1732

--- runtime/userspace/api/caliptra-libapi/src/signer.rs
+++ runtime/userspace/api/caliptra-libapi/src/signer.rs
@@ -2,11 +2,12 @@
 
 use crate::error::{CaliptraApiError, CaliptraApiResult};
 use crate::mailbox_api::execute_mailbox_cmd;
-use crate::ocp_lock::OcpLockSigner;
+use crate::ocp_lock::{EndorsementAlgorithm, OcpLockSigner};
 use alloc::boxed::Box;
 use async_trait::async_trait;
 use caliptra_api::mailbox::{
-    CommandId, MailboxReqHeader, SignWithExportedEcdsaReq, SignWithExportedEcdsaResp,
+    CommandId, MailboxReqHeader, MldsaSignType, SignWithExportedEcdsaReq,
+    SignWithExportedEcdsaResp, SignWithExportedMldsaReq, SignWithExportedMldsaResp,
 };
 use caliptra_mcu_libsyscall_caliptra::dpe_handle_store::{
     DpeHandleStore, DPE_HANDLE_STORE_DRIVER_NUM, EXPORTED_CDI_SIZE,
@@ -24,29 +25,45 @@ pub trait DpeTransport: Send + Sync {
 
 pub struct CaliptraDpeSigner<'a> {
     mailbox: &'a Mailbox,
+    algorithm: EndorsementAlgorithm,
 }
 
 impl<'a> CaliptraDpeSigner<'a> {
     pub fn new(mailbox: &'a Mailbox) -> Self {
-        Self { mailbox }
+        Self {
+            mailbox,
+            algorithm: EndorsementAlgorithm::EcdsaP384Sha384,
+        }
+    }
+
+    pub fn with_algorithm(mailbox: &'a Mailbox, algorithm: EndorsementAlgorithm) -> Self {
+        Self { mailbox, algorithm }
     }
 }
 
 #[async_trait]
-impl<'a> OcpLockSigner for CaliptraDpeSigner<'a> {
+impl OcpLockSigner for CaliptraDpeSigner<'_> {
+    fn algorithm(&self) -> EndorsementAlgorithm {
+        self.algorithm
+    }
+
+    fn signature_size(&self) -> usize {
+        match self.algorithm {
+            EndorsementAlgorithm::EcdsaP384Sha384 => 96,
+            EndorsementAlgorithm::MlDsa87 => SignWithExportedMldsaResp::SIG_SIZE,
+        }
+    }
+
     async fn sign(
         &self,
         _label: &[u8],
         data: &[u8],
         signature: &mut [u8],
     ) -> CaliptraApiResult<()> {
-        if signature.len() < 96 {
+        let sig_size = self.signature_size();
+        if signature.len() < sig_size {
             return Err(CaliptraApiError::InvalidArgBufferTooSmall);
         }
-
-        let digest: [u8; 48] = data
-            .try_into()
-            .map_err(|_| CaliptraApiError::InvalidArgDigestSize)?;
 
         let dpe_store = DpeHandleStore::<DefaultSyscalls>::new(DPE_HANDLE_STORE_DRIVER_NUM);
         let mut exported_cdi = [0u8; EXPORTED_CDI_SIZE];
@@ -58,31 +75,65 @@ impl<'a> OcpLockSigner for CaliptraDpeSigner<'a> {
             return Err(CaliptraApiError::InvalidResponse);
         }
 
-        let mut req = SignWithExportedEcdsaReq {
-            hdr: MailboxReqHeader::default(),
-            exported_cdi_handle: exported_cdi,
-            tbs: digest,
-        };
+        match self.algorithm {
+            EndorsementAlgorithm::EcdsaP384Sha384 => {
+                let digest: [u8; 48] = data
+                    .try_into()
+                    .map_err(|_| CaliptraApiError::InvalidArgDigestSize)?;
 
-        let mut resp_bytes = [0u8; size_of::<SignWithExportedEcdsaResp>()];
-        execute_mailbox_cmd(
-            self.mailbox,
-            CommandId::SIGN_WITH_EXPORTED_ECDSA.into(),
-            req.as_mut_bytes(),
-            &mut resp_bytes,
-        )
-        .await?;
+                let mut req = SignWithExportedEcdsaReq {
+                    hdr: MailboxReqHeader::default(),
+                    exported_cdi_handle: exported_cdi,
+                    tbs: digest,
+                };
 
-        let (resp, _) = SignWithExportedEcdsaResp::read_from_prefix(&resp_bytes)
-            .map_err(|_| CaliptraApiError::InvalidResponse)?;
+                let mut resp_bytes = [0u8; size_of::<SignWithExportedEcdsaResp>()];
+                execute_mailbox_cmd(
+                    self.mailbox,
+                    CommandId::SIGN_WITH_EXPORTED_ECDSA.into(),
+                    req.as_mut_bytes(),
+                    &mut resp_bytes,
+                )
+                .await?;
 
-        signature[0..48].copy_from_slice(&resp.signature_r);
-        signature[48..96].copy_from_slice(&resp.signature_s);
+                let (resp, _) = SignWithExportedEcdsaResp::read_from_prefix(&resp_bytes)
+                    .map_err(|_| CaliptraApiError::InvalidResponse)?;
 
-        Ok(())
-    }
+                signature[0..48].copy_from_slice(&resp.signature_r);
+                signature[48..96].copy_from_slice(&resp.signature_s);
 
-    fn signature_size(&self) -> usize {
-        96
+                Ok(())
+            }
+            EndorsementAlgorithm::MlDsa87 => {
+                if data.len() > SignWithExportedMldsaReq::MAX_TBS_SIZE {
+                    return Err(CaliptraApiError::InvalidArgDigestSize);
+                }
+
+                let mut req = Box::new(SignWithExportedMldsaReq {
+                    hdr: MailboxReqHeader::default(),
+                    exported_cdi_handle: exported_cdi,
+                    sign_type: MldsaSignType::Mu as u32,
+                    tbs_size: data.len() as u32,
+                    tbs: [0u8; SignWithExportedMldsaReq::MAX_TBS_SIZE],
+                });
+                req.tbs[..data.len()].copy_from_slice(data);
+
+                let mut resp_bytes = alloc::vec![0u8; size_of::<SignWithExportedMldsaResp>()];
+                execute_mailbox_cmd(
+                    self.mailbox,
+                    CommandId::SIGN_WITH_EXPORTED_MLDSA.into(),
+                    req.as_mut_bytes(),
+                    &mut resp_bytes,
+                )
+                .await?;
+
+                let (resp, _) = SignWithExportedMldsaResp::read_from_prefix(&resp_bytes)
+                    .map_err(|_| CaliptraApiError::InvalidResponse)?;
+
+                signature[..SignWithExportedMldsaResp::SIG_SIZE].copy_from_slice(&resp.signature);
+
+                Ok(())
+            }
+        }
     }
 }
