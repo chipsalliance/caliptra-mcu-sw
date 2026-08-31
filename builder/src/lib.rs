24d052c4ddd3879bad5a63e4fe32eb31dc05f4f3
Fox Cutter (Dexian) <v-focutter@microsoft.com>
2026-08-31 16:12:45 -0700

    Using copilot to help meet acceptance critera

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -18,9 +18,7 @@ pub use all::{
 };
 pub use caliptra::{AuthManifestOwnerConfig, AuthManifestPubKeysPaths, CaliptraBuilder, ImageCfg};
 pub use network_rom::network_rom_build;
-pub use component_svn_validation::{
-    ComponentSvnEntry, ComponentSvnFuseMapEntry, ComponentSvnValidationConfig,
-};
+pub use component_svn_validation::{ComponentSvnEntry, ComponentSvnValidationConfig};
 pub use offline_signing::*;
 pub use rom::{append_rom_digest, rom_build, rom_size_for_platform, test_rom_build};
 pub use runtime::{bare_metal_build, runtime_build_with_apps};
